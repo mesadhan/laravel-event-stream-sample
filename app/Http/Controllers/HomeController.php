24764e953854f06f689518233b44e5ada69bf506
mesadhan <sadhansarker3@gmail.com>
2019-04-21 01:43:06 +0600

    updated style

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,19 +10,23 @@ class HomeController extends Controller
     public function getEventStream()
     {
 
-        $random_string = chr(rand(65, 90)) . chr(rand(65, 90)) .'-'.  chr(rand(65, 90)) .'-'.  chr(rand(65, 90)) .'-'.  chr(rand(65, 90));
+
+        $nameArray = [
+            'MD. Sadhan Sarker', 'MD. Ripon Sarker', 'Hannan Taluker', 'Korim Islam', 'Reazul Islam',
+        ];
+        $random_name = $nameArray[rand(1, count($nameArray)-1)];
+
+        $random_string = chr(rand(65, 90)) . chr(rand(65, 90)) . '-' . chr(rand(65, 90)) . '-' . chr(rand(65, 90)) . '-' . chr(rand(65, 90));
+
         $data = [
+            'id' => rand(10, 100),
             'time' => date('h:i:s'),
             'message' => $random_string,
-            'id' => rand(10, 100),
-            'name' => 'Sadhan Sarker',
+            'name' => $random_name
         ];
 
-
         $response = new StreamedResponse();
-        $response->headers->set('Content-Type', 'text/event-stream');
-        $response->headers->set('Cache-Control', 'no-cache');
-        $response->setCallback(function () use ($data){
+        $response->setCallback(function () use ($data) {
 
             echo 'data: ' . json_encode($data) . "\n\n";
             //echo "retry: 100\n\n"; // no retry would default to 3 seconds.
@@ -30,12 +34,11 @@ class HomeController extends Controller
             ob_flush();
             flush();
             //sleep(10);
-            usleep(20000);
+            usleep(2000);
         });
+        $response->headers->set('Content-Type', 'text/event-stream');
+        $response->headers->set('Cache-Control', 'no-cache');
         $response->send();
-
-
-
     }
 
 }
--- resources/views/event-stream.blade.php
+++ resources/views/event-stream.blade.php
@@ -1,17 +1,68 @@
+<style>
+    #main_frame {
+        border: 5px solid red;
+        padding: 20px;
+        max-width: 800px;
+        margin: 0 auto;
+    }
 
-<h1 style="text-align: center; margin: 50px;" id="content"></h1>
+    .main_content {
+        text-align: center;
+        margin: 50px;
+    }
+</style>
+
+<h1 class="main_content">Laravel-Event Stream- Server Send Event</h1>
+
+<div id="main_frame">
+    <div> Name: <span id="content_name"></span></div>
+    <div> Time: <span id="content_time"></span></div>
+    <div> ID: <span id="content_id"></span></div>
+    <div> Message: <span id="content_message"></span></div>
+
+    <h3 id="rawData"></h3>
+</div>
 
 
 <script>
 
     let evtSource = new EventSource("/getEventStream", {withCredentials: true});
 
-     evtSource.onmessage = function (e) {
-         let data = JSON.parse(e.data);
-         console.log(data);
+    evtSource.onmessage = function (e) {
+        let data = JSON.parse(e.data);
 
-         let content = document.getElementById('content');
-         content.textContent = JSON.stringify(data);
-     };
+        console.log('EventData:- ', data);
+
+
+        let rawData = document.getElementById('rawData');
+        let main_frame = document.getElementById('main_frame');
+        let content_name = document.getElementById('content_name');
+        let content_time = document.getElementById('content_time');
+        let content_id = document.getElementById('content_id');
+        let content_message = document.getElementById('content_message');
+
+        rawData.textContent = JSON.stringify(data);
+        content_name.textContent = data.name;
+        content_time.textContent = data.time;
+        content_id.textContent = data.id;
+        content_message.textContent = data.message;
+
+        let color = getRandomColor();
+        main_frame.style.borderColor = color;
+        rawData.style.color = color;
+
+
+    };
+
+
+    function getRandomColor() {
+        let letters = '0123456789ABCDEF';
+        let color = '#';
+        for (let i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        console.log('Selected color:- ', color);
+        return color;
+    }
 
 </script>
